64c2d1a98e1325d9e7c2f3b83587668bf77b4eec
Marius Altmann <dodeca@gmx.de>
2020-02-19 20:17:46 +0100

    added processor.lua to the copied files - seems like this has been forgotten

--- conky/setup.sh
+++ conky/setup.sh
@@ -6,6 +6,7 @@ cp conky_shortcuts.conf ~/.config/conky/
 cp conky.conf ~/.config/conky/
 cp conky_weather.conf ~/.config/conky/
 cp weather.lua ~/.config/conky/
+cp processor.lua ~/.config/conky/
 
 # add call of conky to autostart
 echo $'# run conky\nconky -c ~/.config/conky/conky_shortcuts.conf\nconky -c ~/.config/conky/conky_weather.conf\nconky -c ~/.config/conky/conky.conf' >> ~/.config/awesome/autorun.sh
